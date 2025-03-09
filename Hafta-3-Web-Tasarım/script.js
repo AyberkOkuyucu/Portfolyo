dfb7c3465b6080af0c42847733a40eade323ab2a
Viternial <ayberkokuyucu78@gmail.com>
2025-03-10 00:16:42 +0300

    Update script.js

--- Hafta-3-Web-Tasarım/script.js
+++ Hafta-3-Web-Tasarım/script.js
@@ -6,3 +6,28 @@ document.addEventListener("DOMContentLoaded", function() {
       projeContainer.classList.toggle("hidden");
   });
 });
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleButton = document.getElementById("buton2");
+  const body = document.body;
+  
+  if (localStorage.getItem("theme") === "light") {
+      body.classList.add("light-mode");
+  }
+  
+  toggleButton.addEventListener("click", function() {
+      body.classList.toggle("light-mode");
+      
+      if (body.classList.contains("light-mode")) {
+          localStorage.setItem("theme", "light");
+      } else {
+          localStorage.setItem("theme", "dark");
+      }
+  });
+
+  const allTextElements = document.querySelectorAll("body, header, .about-section, .projects-container, footer, aside, .col-3, .box a, .project-title, .icon-box h3, aside ul li a");
+  toggleButton.addEventListener("click", function() {
+      allTextElements.forEach(el => {
+          el.classList.toggle("light-mode-text");
+      });
+  });
+});
